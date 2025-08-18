41c82b746efe1899d1b602cd65477c67a32be12c
Jorge Armando <jorgearmandovdj@gmail.com>
2025-08-18 02:27:31 -0300

    adding response

---
+++ FirstAppMaui.Core/Product/Singleton/ProductCartSingleton.cs
@@ -0,0 +1,24 @@
+﻿using FirstAppMaui.Core.Product.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppMaui.Core.Product.Singleton
+{
+    public class ProductCartSingleton
+    {
+        private ProductCartSingleton() { }
+
+        public static ProductCartSingleton Instance { get; private set; } = new ProductCartSingleton();
+
+        public List<string> ProductIds { get; private set; } = new List<string>();
+
+        public void AddProductId(string productId) {
+            ProductIds.Add(productId);
+            Console.WriteLine(productId);
+        }
+        public void Clear() => ProductIds.Clear();
+    }
+}
