bb93796ce365c0a615143605e73bd7d77fa5b769
Jorge Armando <jorgearmandovdj@gmail.com>
2025-08-18 01:46:38 -0300

    adding response

---
+++ FirstAppMAUI/Views/Content/ProductCardsView.xaml.cs
@@ -0,0 +1,10 @@
+namespace FirstAppMAUI.Views.Content;
+
+public partial class ProductCardsView : ContentView
+{
+	public ProductCardsView()
+	{
+		InitializeComponent();
+	}
+
+} 
---
+++ FirstAppMAUI/Views/Content/SearchHeaderView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FirstAppMAUI.Views.Content;
+
+public partial class SearchHeaderView : ContentView
+{
+	public SearchHeaderView()
+	{
+		InitializeComponent();
+	}
+}
